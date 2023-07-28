6fc3510750dea37768dbd2462fdd27484ba016a2
CariniGuido <103467691+CariniGuido@users.noreply.github.com>
2023-07-28 05:01:54 -0300

    arrancando el front

---
+++ src/components/ListClientesComponent.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from "react/cjs/react.production.min";
+
+
+export  const ListClientesComponent = () => {
+
+const [empleados,setEmpleados] = useState ([]);
+    return  (
+    <div>
+    ListClientesComponent
+    </div>
+    )
+}
+
+export default ListClientesComponent;
