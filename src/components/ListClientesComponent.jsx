27a5849bc837b7a81219ec7cd17fb5fec9a4fb03
CariniGuido <103467691+CariniGuido@users.noreply.github.com>
2023-08-01 02:13:13 -0300

    listo

--- src/components/ListClientesComponent.jsx
+++ src/components/ListClientesComponent.jsx
@@ -5,33 +5,37 @@ import ClienteService from '../services/ClienteService';
 export const ListClientesComponent = () => {
   const [clientes, setClientes] = useState([]);
 
-  useEffect (() => {
-  listarClientes()
-  },[])
+  useEffect(() => {
+    listarClientes();
+  }, []);
 
- const  listarClientes = () => {
-  ClienteService.getAllClientes().then(response=> {
-    setClientes(response.data);
-    console.log (response.data);
-}).catch(error => {
-    console.log (error);
-})
+  const listarClientes = () => {
+    ClienteService.getAllClientes()
+      .then(response => {
+        setClientes(response.data);
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
-
- }
-
-const deleteCliente = (clienteId) => {
-  ClienteService.deleteCliente (clienteId).then((response) => {
-  listarClientes ();
-  }).catch(error => {
-    console.log (error);
-  })
-}
+  const deleteCliente = clienteId => {
+    ClienteService.deleteCliente(clienteId)
+      .then(response => {
+        listarClientes();
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
   return (
     <div className="container">
       <h2 className="text-center">Lista de empleados</h2>
-      <Link to="/add-cliente" className='btn btn-primary mb-2'>Agregar cliente</Link>
+      <Link to="/add-cliente" className="btn btn-primary mb-2">
+        Agregar cliente
+      </Link>
       <table className="table table-borderer table-striped">
         <thead>
           <tr>
@@ -43,19 +47,39 @@ const deleteCliente = (clienteId) => {
           </tr>
         </thead>
         <tbody>
-          {clientes.map(cliente => (
-            <tr key={cliente.id}>
-               <td>{cliente.id}</td>
-              <td>{cliente.nombre}</td>
-              <td>{cliente.apellido}</td>
-              <td>{cliente.email}</td>
-              <td>
-                <Link className='btn btn-info' to={`/edit-cliente/${cliente.id}`}>Actualizar</Link>
-
-                <button style={{marginLeft:"10px"}} className="btn btn-danger" onClick={() => deleteCliente(cliente.id)}>Eliminar cliente</button>
-              </td>
+          {Array.isArray(clientes) ? (
+            clientes.map(cliente => (
+              <tr key={cliente.id}>
+                <td>{cliente.id}</td>
+                <td>{cliente.nombre}</td>
+                <td>{cliente.apellido}</td>
+                <td>{cliente.email}</td>
+                <td>
+                  <Link
+                    className="btn btn-info"
+                    to={`/edit-cliente/${cliente.id}`}
+                  >
+                    Actualizar
+                  </Link>
+                  <button
+                    style={{ marginLeft: '10px' }}
+                    className="btn btn-danger"
+                    onClick={() => deleteCliente(cliente.id)}
+                  >
+                    Eliminar cliente
+                  </button>
+                </td>
+              </tr>
+            ))
+          ) : clientes === null ? (
+            <tr>
+              <td colSpan="5">Cargando...</td>
             </tr>
-          ))}
+          ) : (
+            <tr>
+              <td colSpan="5">Error al cargar los datos.</td>
+            </tr>
+          )}
         </tbody>
       </table>
     </div>
--- src/services/ClienteService.jsx
+++ src/services/ClienteService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const Cliente_BASE_REST_API_URL = "https://fullstackapi.vercel.app/api/v1/clientes";
+const Cliente_BASE_REST_API_URL = "http://localhost:8080/api/v1/clientes";
 
 class ClienteService {
     getAllClientes() {
