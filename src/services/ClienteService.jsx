7c21306c6543d1f07036d97a6e8659743524d1f4
CariniGuido <103467691+CariniGuido@users.noreply.github.com>
2023-08-02 03:21:32 -0300

    listow

--- src/services/ClienteService.jsx
+++ src/services/ClienteService.jsx
@@ -4,23 +4,43 @@ const Cliente_BASE_REST_API_URL = "http://localhost:8080/api/v1/clientes";
 
 class ClienteService {
     getAllClientes() {
-
-        return axios.get(Cliente_BASE_REST_API_URL)
+        return axios.get(Cliente_BASE_REST_API_URL, {
+            headers: {
+                "Access-Control-Allow-Headers": "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+            }
+        });
     }
 
     createCliente(cliente) {
-        return axios.post(Cliente_BASE_REST_API_URL, cliente);
+        return axios.post(Cliente_BASE_REST_API_URL, cliente, {
+            headers: {
+                "Access-Control-Allow-Headers": "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+            }
+        });
     }
+
     getClienteById(clienteId) {
-        return axios.get(Cliente_BASE_REST_API_URL + "/" + clienteId);
+        return axios.get(Cliente_BASE_REST_API_URL + "/" + clienteId, {
+            headers: {
+                "Access-Control-Allow-Headers": "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+            }
+        });
     }
 
-    updateCliente(clienteId,cliente) {
-        return axios.put(Cliente_BASE_REST_API_URL + "/" + clienteId, cliente);
+    updateCliente(clienteId, cliente) {
+        return axios.put(Cliente_BASE_REST_API_URL + "/" + clienteId, cliente, {
+            headers: {
+                "Access-Control-Allow-Headers": "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+            }
+        });
     }
-    deleteCliente (clienteId){
-        return axios.delete(Cliente_BASE_REST_API_URL + "/" + clienteId);
 
+    deleteCliente(clienteId) {
+        return axios.delete(Cliente_BASE_REST_API_URL + "/" + clienteId, {
+            headers: {
+                "Access-Control-Allow-Headers": "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+            }
+        });
     }
 }
 
